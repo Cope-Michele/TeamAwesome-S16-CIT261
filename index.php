9bf47cb40b0a729f956dff269c0a14ce9045a403
Solid89 <kellyfishbackjohnson@gmail.com>
2016-06-20 11:32:35 -0400

    main index.php file
    This will be a good starting point as I have included divs and sections

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="en" class="no-js">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="author" content="Team 5">
+  <link rel="stylesheet" type="text/css" href="/normalize.css">
+  <link rel="stylesheet" type="text/css" href="/style.css">
+  <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script> <!-- are we allowed to use jQuery? -->
+  <script src="modernizer.js"></script> <!-- for browser compatibility checks NOTE: does not fill in the gaps. see youtube "How to use modernizer - SIMPLIFIED" -->
+  <title>Flash Cards</title>
+</head>
+<body>
+  <div class="container">
+    <header>
+      <nav class="">
+        <a href="">1st page</a>
+      </nav>
+      <div class="">
+        <h2>Home</h2>
+        <div class="" id="">
+          <ul>
+            <li class=""><a href=""><span>first item</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  </div> <!--container-->
+  <div class="">
+    <section>
+      <h2></h2>
+      <p></p>
+    </section>
+  </div>
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php';?>
+</body>
+
+</html>
