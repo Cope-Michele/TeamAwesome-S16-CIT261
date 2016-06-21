83c382c85885b0fe8196cdf244f7a68a19f46867
Solid89 <kellyfishbackjohnson@gmail.com>
2016-06-21 15:01:43 -0400

    project
    footer module for simpler code

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+	<div class="">
+		<a href=""><class=""></a>
+		<a href=""><class=""></a>
+		<a href=""><class=""></a>
+	</div>
+	<div class="">
+		<p class="">
+			<a href="" title=""></a>
+		</p>
+</div>
+</footer>
