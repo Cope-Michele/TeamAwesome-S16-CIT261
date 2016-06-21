cd77e48c04d931e1a02a9f3b16149f1d6d244be1
Solid89 <kellyfishbackjohnson@gmail.com>
2016-06-21 14:56:57 -0400

    Project
    added php module for the header.

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="container">
+  <!--	Top Navigation -->
+  <header>
+    <p><img id="logo" src="" alt="logo" width="200" /></p>
+    <h1><span></span></h1>
+    <nav class="">
+      <a href="/">Home</a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+    </nav>
+  </header>
+</div><!-- container -->
