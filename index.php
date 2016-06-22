d1f6fc02c770a8deec94682e44bc7a0cb5114149
Solid89 <kellyfishbackjohnson@gmail.com>
2016-06-22 15:59:21 -0400

    Project

--- index.php
+++ index.php
@@ -1,29 +1,44 @@
 <!DOCTYPE HTML>
 <html lang="en" class="no-js">
-
 <head>
   <meta charset="UTF-8">
   <meta name="author" content="Team 5">
   <link rel="stylesheet" type="text/css" href="/normalize.css">
-  <link rel="stylesheet" type="text/css" href="/style.css">
-  <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script> <!-- are we allowed to use jQuery? -->
   <script src="modernizer.js"></script> <!-- for browser compatibility checks NOTE: does not fill in the gaps. see youtube "How to use modernizer - SIMPLIFIED" -->
+  <link rel="stylesheet" type="text/css" href="style.css">
   <title>Flash Cards</title>
 </head>
 <body>
-  <div class="container">
-    <?php include '/moduled/header.php'; ?> <!-- see header.php for code -->
-    <div id="f1_container">
-      <div id="f1_card" class="shadow">
-        <div class="front face">
-          <p>What is the capital of Indiana?</p>
-        </div>
-        <div class="back face center">
-          <p>Indianapolis is the capital of Indiana.</p>
-          <p>Any content can go here.</p>
-        </div>
-      </div>
-    </div>  
-  <?php include '/moduled/footer.php'; ?> <!-- see footer.php for code -->
+ <?php include '/moduled/header.php'; ?> <!-- see header.php for code -->
+	<div class="content">
+		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+		  <div class="flipper">
+		    <div class="frontIndiana">
+		      <span class="name"><span class="small">What is the capital of </span>Indiana?</span>
+		    </div><!-- front -->
+		    <div class="back">
+		      <div class="back-logo"></div>
+		      <div class="back-title">Indianapolis</div>
+		      <p><i>Santa Claus, Indiana, receives hundreds of thousands of letters addressed to the Christmas legend every year—each of which is responded to individually.</i></p>
+		    </div><!--back-->
+		  </div><!--flipper-->
+		</div><!--flip-container--><br>
+		<button type="submit">Next</button>
+	</div><!--content-->
+	<div class="content">
+		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+		  <div class="flipper">
+		    <div class="frontCalifornia">
+		      <span class="name"><span class="small">What is the capital of </span>California?</span>
+		    </div><!-- front -->
+		    <div class="back">
+		      <div class="back-logo"></div>
+		      <div class="back-title">Sanfrancisco</div>
+		      <p><i>San Francisco's population grew 180 times during the 7 year gold rush.<br> From 200 to over 36,000</i></p>
+		    </div><!--back-->
+		  </div><!--flipper-->
+		</div><!--flip-container-->
+	</div><!-- content -->
+ <?php include '/moduled/footer.php'; ?> <!-- see footer.php for code -->
 </body>
 </html>
